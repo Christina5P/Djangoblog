5b472c7fb98d1065fe1d42cf3bf69d664f5b63ec
Christina Åhman <christina.ahman@tv4.se>
2024-06-08 06:30:44 +0000

    summernote

--- blog/models.py
+++ blog/models.py
@@ -17,30 +17,53 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-   
-class Meta:
-    ordering = ["-created_on"]
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | slug: {self.slug} | written by {self.author}"
 
-
 class Comment(models.Model):
-  post = models.ForeignKey(
-      Post, on_delete=models.CASCADE,
-      related_name="comments"
-  ) 
-  author = models.ForeignKey(
-      User,
-      on_delete=models.CASCADE,
-      related_name="comments_author"
-  )
-  body = models.TextField()
-  approved = models.BooleanField(default=False)
-  created_on = models.DateTimeField(auto_now_add=True)
- 
-class Meta:
-    ordering = ["-created_on"]
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="comments"
+    ) 
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.body} | by {self.author}"
+
+from django.core.exceptions import ValidationError
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def clean(self):
+        try:
+            self.status = int(self.status)
+        except ValueError:
+            raise ValidationError({'status': "Status must be an integer."})
+
+    def __str__(self):
+        return f"{self.title} | slug: {self.slug} | written by {self.author}"
